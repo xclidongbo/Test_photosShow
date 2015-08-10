c600b37f9fd18a98f66708559b20287758314150
xclidongbo <xclidongbo@gmail.com>
2015-08-10 15:25:11 +0800

    imagePicker
    从相册和拍照选择图片.

--- Test_photosShow/ViewController.m
+++ Test_photosShow/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UINavigationControllerDelegate,UIImagePickerControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIImageView *ContentImageView;
+
+@property (strong, nonatomic) UIImagePickerController * imagePickerController;
 
 @end
 
@@ -17,11 +21,61 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _imagePickerController = [[UIImagePickerController alloc] init];
+    
+    _imagePickerController.delegate = self;
+    _imagePickerController.allowsEditing = YES;
+    
+    
+    _ContentImageView.backgroundColor = [UIColor yellowColor];
+
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+
+- (IBAction)actionFromPhotoAlbum:(UIButton *)sender {
+    
+    _imagePickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:_imagePickerController animated:YES completion:^{
+        NSLog(@"相册");
+    }];
+}
+
+
+
+- (IBAction)actionFromCamero:(UIButton *)sender {
+    _imagePickerController.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:_imagePickerController animated:YES completion:^{
+        NSLog(@"相机");
+    }];
+    
+}
+
+#pragma mark - picker delegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    NSLog(@"%@",info);
+    
+    UIImage* image = [info objectForKey:UIImagePickerControllerEditedImage];
+    _ContentImageView.image = image;
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
+    
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
 
 @end
